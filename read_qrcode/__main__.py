978d6d338259cc63533891deb7d5d96846f6881f
jean0t <joaomouryf@hotmail.com>
2025-04-08 14:14:54 -0300

    wayland support available

--- read_qrcode/__main__.py
+++ read_qrcode/__main__.py
@@ -1,4 +1,9 @@
-from .read_qrcode import main
+from .read_qrcode import Qr_code
+
+def main():
+    qrcode = Qr_code()
+    qrcode.output_data()
+
 
 if __name__ == "__main__":
     main()
--- read_qrcode/read_qrcode.py
+++ read_qrcode/read_qrcode.py
@@ -1,10 +1,29 @@
+from subprocess import run
+from os import environ
+from io import BytesIO
+
 from pyautogui import screenshot
 from pyzbar.pyzbar import decode
+from PIL import Image
 
 class Qr_code:
     def __init__(self):
-        self.screen = screenshot()
+        self.screen = self._take_screenshot()
         self.qr_codes = self._decode(self.screen)
+
+    def _take_screenshot(self):
+        session_type = environ.get("XDG_SESSION_TYPE")
+        if session_type.lower() == "x11":
+            return screenshot()
+        
+        elif session_type.lower() == "wayland":
+            screen_image = run(['flameshot', 'screen', '-r'], capture_output=True)
+            
+            if screen_image.returncode == 0:
+                return Image.open(BytesIO(screen_image.stdout))
+            else:
+                print(screen_image.stderr)
+                exit(screen.returncode)
 
     def _decode(self, image):
         return decode(image)
@@ -14,6 +33,3 @@ class Qr_code:
             print(i.data.decode("utf-8"))
 
 
-def main():
-    qrcode = Qr_code()
-    qrcode.output_data()
